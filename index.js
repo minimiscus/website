89afffa6a4a7ad6c0a12e9b577f4ddf31835c549
minimiscus <32967077+minimiscus@users.noreply.github.com>
2025-04-25 19:50:19 +0100

    wow what was i on

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log(
-  `Hey, you found me... well... while you're here try finding some easter eggs using /tomfoolishness/*easter egg here*/ in the URL using the repo to find some interesting stuff (more coming soon).`
-);
-// secret ooo
-
-document.querySelector(`h1`).textContent = `Coming soon!`;
-
-
-
+document.querySelector(`h1`).textContent = "maybe coming sooner or later?";
